6f242323ba8da6abcd13756b58cdc0b7f86563e4
Skomorac <admir.skomorac@outlook.com>
2024-03-14 19:01:31 +0100

    update for skills

--- src/SkillList.js
+++ src/SkillList.js
@@ -1,22 +1,53 @@
 import React from "react";
 import "./styles.css";
 
+const skills = [
+  {
+    skill: "HTML + CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "intermediate",
+    color: "#EFD81D",
+  },
+  {
+    skill: "REACT.JS",
+    level: "beginner",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Bootstrap",
+    level: "intermediate",
+    color: "#C3DCAF",
+  },
+];
+
 function SkillList() {
   return (
     <div className="skill-list">
-      <Skill skill="HTML + CSS" emoji="🐎" color="blue" />
-      <Skill skill="Javascript" emoji="🤖" color="yellow" />
-      <Skill skill="React" emoji="🌐" color="orangered" />
-      <Skill skill="Bootstrap" emoji="🎨" color="aqua" />
+      {skills.map((skill, index) => (
+        <Skill
+          key={index}
+          skill={skill.skill}
+          level={skill.level}
+          color={skill.color}
+        />
+      ))}
     </div>
   );
 }
 
-function Skill(props) {
+function Skill({ skill, level, color }) {
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
     </div>
   );
 }
